2e0c471f61b348aed0007f204399be38897ea9e9
Jean-Quenault <jeanform@proton.me>
2023-12-20 18:57:07 +0100

    Api gateway back domain and Route53 domain (backend)

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -22,3 +22,29 @@ resource "aws_api_gateway_deployment" "prod" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = "prod"
 }
+
+data "aws_acm_certificate" "certificate_backend_domain_arn" {
+  domain   = "backend.jeanops.net"
+  statuses = ["ISSUED"]
+}
+
+resource "aws_api_gateway_domain_name" "backend" {
+  domain_name              = "backend.jeanops.net"
+  certificate_arn          = aws_acm_certificate.cert.arn
+
+}
+
+resource "aws_api_gateway_base_path_mapping" "mapping" {
+  api_id      = aws_api_gateway_rest_api.api.id
+  stage_name  = aws_api_gateway_stage.mystage.prod
+  domain_name = aws_api_gateway_domain_name.backend.domain_name
+
+}
+
+resource "aws_route53_record" "api_dns" {
+  zone_id = data.aws_route53_zone.myzone.id
+  name    = "backend.jeanops.net"
+  type    = "CNAME"
+  records = [aws_api_gateway_domain_name.my_domain.cloudfront_domain_name]
+  ttl     = "300"
+}
