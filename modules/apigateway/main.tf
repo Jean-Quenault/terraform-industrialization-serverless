f23349f7c3f0e682ea882362e657ff3443fdfd9a
Jean-Quenault <jeanform@proton.me>
2023-12-21 12:34:02 +0100

    Root zone debug

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -48,13 +48,12 @@ resource "aws_api_gateway_domain_name" "backend_domain" {
 
 }
 
-data "aws_route53_zone" "api_dns" {
-  name = "backend.jeanops.net"
+data "aws_route53_zone" "root_zone" {
+  name = "jeanops.net"
 }
 
-
 resource "aws_route53_record" "api_dns" {
-  zone_id = data.aws_route53_zone.api_dns.id
+  zone_id = data.aws_route53_zone.root_zone.id
   name    = "backend.jeanops.net"
   type    = "CNAME"
   records = [aws_api_gateway_domain_name.backend_domain.cloudfront_domain_name]
