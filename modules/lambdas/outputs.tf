398dfd8a0e2a5bf58215882be377458d6957026e
Jean-Quenault <jeanform@proton.me>
2023-12-26 11:49:12 +0100

    Add the lambdas functions in outputs.tf in lambda module

--- modules/lambdas/outputs.tf
+++ modules/lambdas/outputs.tf
@@ -9,3 +9,15 @@ output "get_users_prod_lambda_arn" {
 output "post_users_prod_lambda_arn" {
   value = aws_lambda_function.post_users_prod.arn
 }
+
+output "get_status_prod_function_name" {
+  value = aws_lambda_function.get_status_prod.function_name
+}
+
+output "get_users_prod_function_name" {
+  value = aws_lambda_function.get_users_prod.function_name
+}
+
+output "post_users_prod_function_name" {
+  value = aws_lambda_function.post_users_prod.function_name
+}
